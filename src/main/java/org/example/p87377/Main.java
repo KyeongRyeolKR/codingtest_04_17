fbe4b250187c1946ac1d6c6255cca328b9eed83a
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-17 14:02:42 +0900

    리팩토링

--- src/main/java/org/example/p87377/Main.java
+++ src/main/java/org/example/p87377/Main.java
@@ -1,5 +1,7 @@
 package org.example.p87377;
 
+import java.util.Objects;
+
 public class Main {
 }
 
@@ -9,27 +11,60 @@ class Solution {
         return answer;
     }
 
-    public long[] intersection(int[] line1, int[] line2) {
-        long A = line1[0];
-        long B = line1[1];
-        long E = line1[2];
+    public Point intersection(int[] line1, int[] line2) {
+        double A = line1[0];
+        double B = line1[1];
+        double E = line1[2];
 
-        long C = line2[0];
-        long D = line2[1];
-        long F = line2[2];
+        double C = line2[0];
+        double D = line2[1];
+        double F = line2[2];
+
+        double divisor = A * D - B * C;
 
         // 분모가 0일 경우, 평행이여서 교점이 없다.
-        if(A * D - B * C == 0) {
+        if(divisor == 0) {
             return null;
         }
 
-        double x = (double) (B * F - E * D) / (A * D - B * C);
-        double y = (double) (E * C - A * F) / (A * D - B * C);
+        double x = (B * F - E * D) / divisor;
+        double y = (E * C - A * F) / divisor;
 
         // 정수인지 아닌지 판별
         if(x != (long) x) return null;
         if(y != (long) y) return null;
 
-        return new long[]{(long) x, (long) y};
+        return Point.of(x, y);
+    }
+}
+
+class Point {
+    public final long x;
+    public final long y;
+
+    private Point(long x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point of(long x, long y) {
+        return new Point(x, y);
+    }
+
+    public static Point of(double x, double y) {
+        return of((long) x, (long) y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
