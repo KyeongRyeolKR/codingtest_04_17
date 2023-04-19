b8fd17f08776cc4de8c5d115558f6e1333ea9ab6
KyeongRyeolKR <rlarudfuf802@naver.com>
2023-04-19 11:10:59 +0900

    하노이의 탑 시작

---
+++ src/main/java/org/example/p12946/Solution.java
@@ -0,0 +1,10 @@
+package org.example.p12946;
+
+class Solution {
+    public int[][] solution(int n) {
+        if (n == 1) return new int[][]{{1, 3}};
+        if (n == 2) return new int[][]{{1, 2}, {1, 3}, {2, 3}};
+
+        return null;
+    }
+}
